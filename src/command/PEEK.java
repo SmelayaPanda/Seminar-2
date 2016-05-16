d8c6c41bf0eb5c5a6d88c622dc77b2006796a3eb
SmelayaPanda <smelayapanda@mail.ru>
2016-05-16 21:15:40 +0600

    add InfoMessage Class

--- src/command/PEEK.java
+++ src/command/PEEK.java
@@ -6,7 +6,7 @@ public class PEEK implements Cmd {
     @Override
     public Stack<Double> exec(Stack<Double> stack) {
         Double v = stack.peek();
-        System.out.println("peek ->" + v);
+        System.out.println("peek -> " + v);
         return stack;
     }
 }
--- src/start/Main.java
+++ src/start/Main.java
@@ -36,20 +36,7 @@ public class Main {
             try {
                 inst = Class.forName(clsName).newInstance();
             } catch (ClassNotFoundException e) {
-                System.err.println("Available commands:" +
-                        "\n__________________________________________________________________" +
-                        "\n PUSH v      ->  put 'v' on top of the stack (v - real number) " +
-                        "\n DEFINE a v  ->  define variable with name 'a'" +
-                        "\n VARPUSH a   ->  put on top of the stack defined variable 'a'" +
-                        "\n POP         ->  removes the top element of the stack" +
-                        "\n PEEK        ->  it shows the top element of the stack" +
-                        "\n__________________________________________________________________" +
-                        "\n MATHEMATICAL OPERATION:" +
-                        "\n SUM         ->  returns the sum of two top elements of the stack" +
-                        "\n DIF         ->  returns the difference of two top elements of the stack" +
-                        "\n DIV         ->  returns the division of two top elements of the stack" +
-                        "\n MUL         ->  returns the multiply of two top elements of the stack" +
-                        "\n SQRT        ->  returns the square root of the top element of the stack");
+                InfoMessage.getInfo();
             }
 
             if ((inst instanceof Cmd)) {
